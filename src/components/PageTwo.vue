49ad97fbc8122b8c6fadf57094779cf87344ae48
Chiu Yung <1837060380@qq.com>
2019-01-31 13:44:57 +0800

    no message

--- src/components/PageTwo.vue
+++ src/components/PageTwo.vue
@@ -5,9 +5,16 @@
 </template>
 
 <script>
-    export default {
-        name: "PageTwo"
+  import axios from 'axios'
+  export default {
+    name: "PageTwo",
+    created(){
+      axios.get('http://127.0.0.1:3000/test', {
+      }).then(function (response) {
+        console.log(response);
+      })
     }
+  }
 </script>
 
 <style scoped>
